b3dc6b35fb7a7af53ecb1f071e4aba620245a64e
kaladin9017 <sanchochris@gmail.com>
2017-05-08 17:39:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const Twit = require('twit');
+const bot = new Twit({
+  consumer_key:process.env.LEARNINGBOT_CONSUMER_KEY,
+  consumer_secret:process.env.LEARNINGBOT_CONSUMER_SECRET,
+  access_token:process.env.LEARNINGBOT_ACCESS_TOKEN,
+  access_token_secret:process.env.LEARNINGBOT_ACCESS_TOKEN_SECRET,
+  timeout_ms:60*1000,
+});
+
+// bot.post('statuses/update', {status: 'hello world!'},
+//   function(err, data, response){
+//     if (err) {
+//       console.log(err);
+//     }
+//     else {
+//       console.log(data.text + 'was twetted')
+//     }
+//   });
+
+bot.get('followers/list', {screen_name: 'bots_with_han'},
+  function(err, data, response) {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.log(data)
+    }
+  })
